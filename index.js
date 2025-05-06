937beb88d99be270321e5083c94c830860281c54
xuannhj <xuannhi.nguyenpham@gmail.com>
2025-05-06 13:14:50 +0700

    add reponsive n js

---
+++ index.js
@@ -0,0 +1,6 @@
+let toggle = document.getElementById("toggle");
+let mobileNav = document.getElementById("mobile-nav");
+
+toggle.addEventListener("click", function () {
+  mobileNav.classList.toggle("active");
+});
